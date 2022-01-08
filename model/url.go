7e48c727b67528bf5693604972f1da02390f8c0c
atk81 <ashutoshkushwaha81@gmail.com>
2022-01-08 23:10:30 +0530

    Added url model

---
+++ model/url.go
@@ -0,0 +1,33 @@
+package model
+
+import "log"
+
+type UrlShortner struct {
+	Id      int64 `json:"id" gorm:"primary_key;auto_increment;not_null"`
+	LongURL string
+}
+
+func AddNewUrl(longURL string) (int64, error) {
+	dbClient := GetDB()
+	urlShortner := UrlShortner{
+		LongURL: longURL,
+	}
+	result := dbClient.DB.Create(&urlShortner)
+	if result.Error != nil {
+		log.Fatalf("Some error occured %v", result.Error)
+		return -1, result.Error
+	}
+	return urlShortner.Id, nil
+}
+
+func GetLongURL(id int64) (string, error) {
+	dbClient := GetDB()
+	var urlShortner UrlShortner
+	result := dbClient.DB.First(&urlShortner, id)
+	if result.Error != nil {
+		log.Fatalf("Some unexpected error occured %v", result.Error)
+		return "", result.Error
+	}
+	return urlShortner.LongURL, nil
+
+}
